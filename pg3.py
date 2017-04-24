bacfbcf1ebd2ff66da088c995bfa0766241980f5
Zeljko Vrbaski <zeljko.vrbaski@sbgenomics.com>
2017-04-24 10:09:30 +0200

    update, delete, select examples

--- pg3.py
+++ pg3.py
@@ -1,8 +1,7 @@
 import pg8000
 
-
-def program():
-    conn = pg8000.connect(
+def connect():
+    return pg8000.connect(
         user='tweet',
         password='tweet',
         host='localhost',
@@ -10,24 +9,72 @@ def program():
         database='pg_example'
     )
 
+
+def create_table(conn):
+    print('\n*****CREATING TABLE*****\n')
+
+    # CREATE TABLE
     cursor = conn.cursor()
     cursor.execute("CREATE TEMPORARY TABLE book (id SERIAL, title TEXT)")
-    cursor.execute(
-        "INSERT INTO book (title) VALUES (%s), (%s)",
-        ("Ender's Game", "Speaker for the Dead"))
 
     cursor.execute(
-        "SELECT id, title FROM book"
+        "INSERT INTO book (title) VALUES (%s), (%s), (%s) RETURNING "
+        "id, title",
+        ("Ender's Game", "Lord of the Rings", "Wheel of Time")
     )
-    results = cursor.fetchall()
-    for row in results:
+
+    res = cursor.fetchone()
+    while res:
+        print(res[0], res[1])
+        res = cursor.fetchone()
+    cursor.close()
+
+
+def update(conn):
+    print('\n*****UPDATE*****\n')
+    cursor = conn.cursor()
+    cursor.execute(
+        "UPDATE book SET title=%s WHERE title LIKE '%%Game%%' RETURNING id",
+        ("A Song of Ice and Fire",)
+    )
+    print('ROWCOUNT:', cursor.rowcount)
+    res = cursor.fetchall()
+    for row in res:
+        print('updated id:', row[0])
+    cursor.close()
+
+
+def delete(conn):
+    print('\n*****DELETE*****\n')
+    cursor = conn.cursor()
+    cursor.execute(
+        "DELETE from book WHERE title IN ('A Song of Ice and Fire', 'Hobbit')",
+    )
+    print('ROWCOUNT:', cursor.rowcount)
+    cursor.close()
+
+
+def select(conn):
+    print('\n*****SELECT*****\n')
+    cursor = conn.cursor()
+    cursor.execute(
+        "SELECT id, title FROM book ORDER BY id"
+    )
+    # results = cursor.fetchall()
+
+    for row in cursor: # it implements __iter__()
         id, title = row
         print("id = %s, title = %s" % (id, title))
-
     cursor.close()
-    conn.commit()
-    conn.close()
 
 
 if __name__ == '__main__':
-    program()
+    conn = connect()
+    create_table(conn)
+    select(conn)
+    update(conn)
+    select(conn)
+    delete(conn)
+    select(conn)
+    conn.commit()
+    conn.close()
